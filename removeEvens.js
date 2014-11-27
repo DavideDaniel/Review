2e523fd9c7da391f471c9220b68bc26f3d1b83d2
DavideDaniel <David@Davids-MBP.home>
2014-11-27 17:08:36 -0500

    Remove Evens w/ another example

--- removeEvens.js
+++ removeEvens.js
@@ -1,8 +1,9 @@
 // function removeEvens
 // takes an array of numbers
 // returns a new array of numbers that has the even numbers removed
-var a = [3, 4, 5, 6, 20, 11, 12, 13, 353, 45664, 545];
+var a = [3, 4, 5, 6, 20, 11];
 var b = removeEvens(a);
+var m = [342, 23, 34, 21, 2123, 3, 4, 5, 6, 7, 8];
 // b now equals [3,5,11]
 function removeEvens(x) {
     var y = [];
@@ -13,4 +14,6 @@ function removeEvens(x) {
     }
     console.log(y); // console.log needs to be in the same scope as the variable - putting it here returns 1 array filled variables
 }
+
 b;
+removeEvens(m);
